d5cb2fb63a03c4c851647b625fe62f2a81dfa206
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-04-10 00:06:56 +0530

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: {
-    // For Tailwind CSS v4, you MUST use the separate plugin package
-    '@tailwindcss/postcss': {}, // <-- Use this package name
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+};
